12ee9b2278ad61c9e0c08cc8b51ed280f9c4da56
xamfy <spratyaksh22@gmail.com>
2019-02-24 04:05:56 +0530

    Consumers work

--- automate/settings.py
+++ automate/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'channels',
+    'channels_redis',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,4 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
 ASGI_APPLICATION = "automate.routing.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('localhost', 6379)],
+        },
+    },
+}
--- main/consumers.py
+++ main/consumers.py
@@ -1,27 +1,94 @@
 import asyncio
 import json
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
 from django.core import serializers
 
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
 from .models import Device
 
 
-class DeviceConsumer(AsyncConsumer):
+class DeviceConsumer(AsyncWebsocketConsumer):
     async def websocket_connect(self, event):
         print("connected", event)
-        await self.send({
-            "type": "websocket.accept"
-        })
+
+        me = self.scope['user']
+        objects = await self.get_devices(me)
+        # print(me)
+        # print(objects)
         # await asyncio.sleep(10)
-        await self.send({
-            "type": "websocket.send",
-            "text": serializers.serialize('json', Device.objects.all(), fields=('name', 'status'))
-        })
+        # await self.send({
+        #     "type": "websocket.send",
+        #     "text": serializers.serialize('json', Device.objects.all(), fields=('name', 'status'))
+        # })
+        user_room = f"room_{me}"
+        print(f"room:{user_room}")
+        self.user_room = user_room
+
+        await self.channel_layer.group_add(
+            user_room,
+            self.channel_name
+        )
+
+        # await self.send({
+        #     "type": "websocket.accept"
+        # })
+        await self.accept()
 
     async def websocket_receive(self, event):
         print("receive", event)
+        # print(event['text'])
+        objects = await self.set_device_status(event['text'], self.scope['user'])
+        # print(objects)
+        data = event.get('text', None)
+        if data is not None:
+            dict_data = json.loads(data)
+            name = dict_data['name']
+            status = dict_data['status']
+            print(f"{name} : {status}")
+            response = {
+                'name': name,
+                'status': status
+            }
+            # new_event = {
+            #     "type": "device_status",
+            #     "text": json.dumps(response)
+            # }
+            # await self.send({
+            #     "type": "websocket.send",
+            #     "text": json.dumps(response)
+            # })
+        # new_event = {
+        #     "type": "websocket.send",
+        #     "text": "hi"
+        # }
+            await self.channel_layer.group_send(
+                self.user_room,
+                {
+                    'type': 'device_status',
+                    'text': json.dumps(response)
+                }
+            )
+
+    async def device_status(self, event):
+        # message = event['text']
+
+        print('message', event['text'])
+        await self.send(text_data=json.dumps(event['text']))
+        # await self.send(text_data=json.dumps({
+        #     'text': message
+        # }))
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
+
+    @database_sync_to_async
+    def get_devices(self, user):
+        return User.objects.filter(username=user).values_list('devices', flat=True)
+
+    @database_sync_to_async
+    def set_device_status(self, status, user):
+        return Device.objects.filter(owner=user)
